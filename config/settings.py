9d4d33edafbb362cf6cab82fe3b110d3eb990504
Mikhail Murzakov <121337665+GreedIsGoody@users.noreply.github.com>
2026-08-20 15:46:39 +0300

    feat: accounts app integration

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.accounts.apps.AccountsConfig',
 ]
+AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
